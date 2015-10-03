479f72988671914386dd93b01d6e43c2c4321fcb
ashleyjihye4 <athomas2@wellesley.edu>
2015-10-03 18:03:26 -0400

    did lots of tasks stuff

--- jac.js
+++ jac.js
@@ -1,17 +1,24 @@
+//EVERYTHING WORKS EXCEPT FOR DATE
+
 Tasks = new Mongo.Collection("tasks");
+People = new Mongo.Collection("people");
 
 if (Meteor.isClient) {
   Meteor.subscribe("tasks");
+  Meteor.subscribe("people");
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
       // Show newest tasks at the top
       return Tasks.find({}, {sort: {dueDate: 1}});
+    },
+    people: function() {
+      return People.find();
     }
   });
 
   Template.body.events({
-    "submit .new-task": function (event, template) {
+    "submit .new-task": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
 
@@ -19,9 +26,14 @@ if (Meteor.isClient) {
       var elem = event.target;
 
       Meteor.call("addTask", elem);
+    },
+    
+    "click #add": function(e) {
+      e.preventDefault();
 
-      // Clear form
-      template.find("form").reset();
+      $(':input:not(:button)', $('#taskModal')[0]).val([]);
+      Session.set('selectedTaskId', null);
+      $('#taskModal').modal('show');
     }
   });
 
@@ -32,32 +44,141 @@ if (Meteor.isClient) {
     },
     "click .delete": function () {
       Meteor.call("deleteTask", this._id);
+    },
+    'click #edit': function(e) {
+      e.preventDefault();
+
+      $(':input:not(:button)', $('#taskModal')[0]).val([]);
+      task = $(e.target).closest('.task')
+      taskId = task.attr('data-id')
+      Session.set('selectedTaskId', taskId);
+
+      $('#taskModal').modal('show');
     }
   });
+
+  Template.taskTemplate.events({
+    'click #save': function(e) {
+
+      var taskId = Session.get('selectedTaskId');
+      Session.set('selectedTaskId', null);
+
+      var task = {
+        name: $('#name').val(),
+        public: $('#public').val(),
+        startDate: $('#start-date').val(),
+        dueDate: $('#due-date').val(),
+        value: $('#value').val(),
+        notes: $('#notes').val()
+      }
+
+      if (!taskId) {
+      Meteor.call('addTask', task, function(error, result) {
+        if (error) {
+          alert(error);
+        }
+      });
+    } else {
+      _.extend(task, {id: taskId});
+      Meteor.call('editTask', task, function(error, result) {
+        if (error) {
+          alert(error);
+        }
+      });
+    }
+
+    $('#taskModal').modal('hide');
+    }
+  });
+
+  Template.taskTemplate.helpers({
+  task: function() {
+    var taskId = Session.get('selectedTaskId');
+    
+    if (typeof taskId !== "undefined") {
+      var task = Tasks.findOne(taskId);
+      return task;
+    } else {
+      return {name:'', public:false, startDate:null, dueDate:null, value:'', notes:'', completed:false};
+    }
+  }
+});
+
+/*
+Template.taskModal.events({
+  'click #save': function(e) {
+    e.preventDefault();
+    
+    var taskId = Session.get('selctedTaskId');
+    var task = {
+      name: $('#taskName').val()
+    }
+
+    if (!taskId) {
+      Meteor.call('addTask', task, function(error, result) {
+        if (error) {
+          alert(error);
+        }
+      });
+    } else {
+      _.extend(task, {id: taskId});
+      Meteor.call('editTask', task, function(error, result) {
+        if (error) {
+          alert(error);
+        }
+      });
+    }
+
+    Modal.hide('taskModal');
+  }
+});
+*/
 }
 
 Meteor.methods({
-  addTask: function (elem) {
+  addTask: function (task) {
     // Make sure the user is logged in before inserting a task
+
+    /*
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
+    */
 
       Tasks.insert({
-        name: elem[0].value,
-        startDate: elem[1].value,
-        dueDate: elem[2].value,
-        value: elem[3].value,
-        notes: elem[4].value,
-        createdAt: new Date(), // current time
-        owner: Meteor.userId()
+        name: task.name,
+        public: task.public,
+        startDate: task.startDate,
+        dueDate: task.dueDate,
+        value: task.value,
+        notes: task.notes,
+        completed: false,
+        createdAt: new Date() // current time
       });
-
   },
+
+  editTask: function(task) {
+    Tasks.update(task.id, {$set: {
+      name: task.name,
+      public: task.public,
+      startDate: task.startDate,
+      dueDate: task.dueDate,
+      value: task.value,
+      notes: task.notes,
+      completed: false
+    }});
+  },
+
   deleteTask: function (taskId) {
     Tasks.remove(taskId);
   },
   setChecked: function (taskId, setChecked) {
-    Tasks.update(taskId, { $set: { checked: setChecked} });
+    Tasks.update(taskId, { $set: { completed: setChecked} });
   }
+  /*
+  getTasks: function (personId) {
+    People.find({_id:personId});
+  }
+  */
+
 });
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('tasks', function() {
     return Tasks.find();
 });
+
+Meteor.publish('people', function() {
+	return People.find();
+});
