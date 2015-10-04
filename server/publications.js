58fa761f52f236f01489906c62fe7b677207b58a
recrudescence <calvinmwang@gmail.com>
2015-10-04 02:13:18 -0400

    facebook graph stuff for social

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,16 @@ Meteor.publish('tasks', function() {
     return Tasks.find();
 });
 
-Meteor.publish('people', function() {
-	return People.find();
+Meteor.publish('userData', function() {
+	if (!this.userId) {
+		return null;
+	}
+	else {
+		return Meteor.users.find({_id: this.userId}, {fields: {
+			debt: 1,
+			'tasks': 1,
+			'overdueTasks': 1,
+			'services.facebook': 1
+		}});
+	}
 });
